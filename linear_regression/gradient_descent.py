f5858b166f477b78afed57cd1bf858add4151fd4
zhizhang.teng <zhizhang.teng@shopee.com>
2021-10-10 13:15:54 +0800

    gradient descent

--- linear_regression/gradient_descent.py
+++ linear_regression/gradient_descent.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def start():
-    print('hello')
+    print("start")
     x_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    y_train = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
+    y_train = np.array([2.1, 3, 4, 5.5, 6, 7, 8, 8.8, 10.1])
     print(x_train)
     print(y_train)
 
@@ -12,6 +12,21 @@ def start():
     w1 = np.random.normal()
     err = 1
     print(w0, w1, err)
+
+    count = 10000
+    rate = 0.001
+    for i in range(count):
+        for x, y in zip(x_train, y_train):
+            w0 = w0 - rate * (h(w0, w1, x) - y) * 1
+            w1 = w1 - rate * (h(w0, w1, x) - y) * x
+        err = 0.0
+        for x, y in zip(x_train, y_train):
+            err += (y - h(w0, w1, x)) ** 2
+        m = len(x_train)
+        err = float(err / (2 * m))
+        print("(%f, %f)\terr: %f" % (w0, w1, err))
+
+    print(w0, w1)
 
 
 def h(w0, w1, x):
